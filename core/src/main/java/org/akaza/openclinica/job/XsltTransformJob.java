69287cfda45582773f283216575c13326349e7de
Zique Wood-Ferren <iwoodferren@openclinica.com>
2024-07-12 13:13:50 -0400

    OC-22988: Add set property to transformer to utf-16

--- core/src/main/java/org/akaza/openclinica/job/XsltTransformJob.java
+++ core/src/main/java/org/akaza/openclinica/job/XsltTransformJob.java
@@ -274,7 +274,7 @@ public class XsltTransformJob extends QuartzJobBean {
 
 
                 endFileStream = new FileOutputStream(endFile);
-                transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+                transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-16");
                 transformer.transform(new StreamSource(xmlFilePath), new StreamResult(endFileStream));
 
                 // JN...CLOSE THE STREAM...HMMMM
